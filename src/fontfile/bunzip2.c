8a512b3945986e52aa6c11caac9212f26dd484cb
Skerlet Development Team <skerlet@swc.toshiba.co.jp>
2014-02-12 12:37:09 +0900

    git-debimport libxfont_1.4.5-2.diff.gz

--- src/fontfile/bunzip2.c
+++ src/fontfile/bunzip2.c
@@ -2,33 +2,28 @@
    written by Mark Eichin <eichin@kitten.gen.ma.us> September 1996.
    intended for inclusion in X11 public releases. */
 
-/* Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+/*
+ * Copyright (c) 2008, Oracle and/or its affiliates. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, and/or sell copies of the Software, and to permit persons
- * to whom the Software is furnished to do so, provided that the above
- * copyright notice(s) and this permission notice appear in all copies of
- * the Software and that both the above copyright notice(s) and this
- * permission notice appear in supporting documentation.
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT
- * OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * HOLDERS INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL
- * INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING
- * FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
- * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
- * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
  *
- * Except as contained in this notice, the name of a copyright holder
- * shall not be used in advertising or otherwise to promote the sale, use
- * or other dealings in this Software without prior written authorization
- * of the copyright holder.
- */ 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
 
 #include "config.h"
@@ -81,7 +76,7 @@ BufFilePushBZIP2 (BufFilePtr f)
 			 BufBzip2FileClose);
 }
 
-static int 
+static int
 BufBzip2FileClose(BufFilePtr f, int flag)
 {
     xzip_buf *x = (xzip_buf *)f->private;
@@ -91,15 +86,15 @@ BufBzip2FileClose(BufFilePtr f, int flag)
     return 1;
 }
 
-/* here's the real work. 
+/* here's the real work.
    -- we need to put stuff in f.buffer, update f.left and f.bufp,
    then return the first byte (or BUFFILEEOF).
-   -- to do this, we need to get stuff into avail_in, and next_in, 
+   -- to do this, we need to get stuff into avail_in, and next_in,
    and call BZ2_bzDecompress appropriately.
    -- we may also need to add CRC maintenance - if BZ2_bzDecompress tells us
    BZ_STREAM_END, we then have 4bytes CRC and 4bytes length...
 */
-static int 
+static int
 BufBzip2FileFill (BufFilePtr f)
 {
     xzip_buf *x = (xzip_buf *)f->private;
@@ -153,7 +148,7 @@ BufBzip2FileFill (BufFilePtr f)
 	}
     }
     f->bufp = x->b;
-    f->left = BUFFILESIZE - x->z.avail_out;  
+    f->left = BUFFILESIZE - x->z.avail_out;
 
     if (f->left >= 0) {
 	f->left--;
@@ -164,7 +159,7 @@ BufBzip2FileFill (BufFilePtr f)
 }
 
 /* there should be a BufCommonSkip... */
-static int 
+static int
 BufBzip2FileSkip (BufFilePtr f, int c)
 {
     /* BufFileRawSkip returns the count unchanged.
